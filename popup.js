d9164fc900203690b7b223c1cd604e5c719514d5
elicymerman <elicymerman@deedef4c-c4cc-42fd-4688-ae2ef018a546>
2014-07-28 13:21:24 +0000

    1.6.2 - fix alt-z not working

--- popup.js
+++ popup.js
@@ -72,7 +72,7 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         advice.style.display = advice.style.display == 'block' ? 'none' : 'block';
     };
     document.getElementById('block-flash').onclick = function () {
-        alert("In order to avoid automatically showing Flash animations, in the 'Plug-ins' section of Chrome's settings (which you will see when you press OK), click either 'Click to play' or 'Block all'.\n'Click to play' is recommended, in case you encounter a Flash animation that is important for you to see.");
+        alert("In order to avoid automatically showing Flash animations, in the 'Plug-ins' section of Chrome's settings (which you will see when you press OK), click either 'Click to play' (recommended), or 'Block all'.");
         chrome.tabs.create({ url: "chrome://settings/content#handlers-section" });
     };
     document.getElementById('report').onclick = function () {
