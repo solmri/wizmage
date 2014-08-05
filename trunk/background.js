50440b3040bacf4c9ddbbf5f0c1385ecfadb6bc2
elicymerman <elicymerman@deedef4c-c4cc-42fd-4688-ae2ef018a546>
2014-08-05 22:32:58 +0000

    2.1.0 - inject js into iframes without src (for twitter widgets), detect more images, revise code

--- trunk/background.js
+++ trunk/background.js
@@ -1,68 +1,100 @@
-var ul = localStorage.urlList, urlList = ul ? JSON.parse(ul) : [], excDomains = [],
-    paused = localStorage.isPaused == 1, isNoPattern = localStorage.isNoPattern == 1, isNoEye = localStorage.isNoEye == 1,
-    pausedTabs = [];
+var ul = localStorage.urlList, urlList = ul ? JSON.parse(ul) : [],
+    paused = localStorage.isPaused == 1,
+    isNoPattern = localStorage.isNoPattern == 1,
+    isNoEye = localStorage.isNoEye == 1,
+    excludeForTabList = [],
+    pauseForTabList = [],
+    domainRegex = /^\w+:\/\/([\w\.:-]+)/;
+function getDomain(url) {
+    var regex = domainRegex.exec(url);
+    return regex ? regex[1].toLowerCase() : null;
+}
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        if (request.r == 'isExceptionOrPaused') {
-            var r = false;
-            if (sender.tab) {
-                if (paused || pausedTabs.indexOf(sender.tab.id) != -1)
-                    r = true;
-                else {
-                    var relevantExcDomains = [];
-                    for (var i = 0; i < excDomains.length; i++) {
-                        if (excDomains[i].tabId == sender.tab.id)
-                            relevantExcDomains.push(excDomains[i].domain);
-                    }
-                    if (relevantExcDomains.length && relevantExcDomains.indexOf(request.location.hostname) != -1)
-                        r = true;
-                    else {
-                        var lowerHref = request.location.href.toLowerCase();
+        switch (request.r) {
+            case 'getSettings':
+                var settings = {
+                    isPaused: paused,
+                    isNoPattern: isNoPattern,
+                    isNoEye: isNoEye
+                };
+                var tab = request.tab || sender.tab;
+                if (tab) {
+                    if (pauseForTabList.indexOf(tab.id) != -1)
+                        settings.isPausedForTab = true;
+                    if (tab.url) {
+                        var domain = getDomain(tab.url);
+                        if (domain) {
+                            for (var i = 0; i < excludeForTabList.length; i++) {
+                                if (excludeForTabList[i].tabId == tab.id && excludeForTabList[i].domain == domain)
+                                { settings.isExcludedForTab = true; break; }
+                            }
+                        }
+                        var lowerUrl = tab.url.toLowerCase();
                         for (var i = 0; i < urlList.length; i++) {
-                            if (lowerHref.indexOf(urlList[i]) != -1) { r = true; break; }
+                            if (lowerUrl.indexOf(urlList[i]) != -1)
+                            { settings.isExcluded = true; break; }
                         }
                     }
                 }
-                if (!r)
-                    chrome.browserAction.setIcon({ path: 'icon.png', tabId: sender.tab.id });
-            }
-            sendResponse(r);
-        } else if (request.r == 'setUrlList') {
-            urlList = request.urlList;
-            localStorage.urlList = JSON.stringify(urlList);
-        } else if (request.r == 'getUrlList')
-            sendResponse(urlList);
-        else if (request.r == 'excTab') {
-            if (request.exclude)
-                excDomains.push({ tabId: request.tabId, domain: request.domain });
-            else
-                excDomains = excDomains.filter(function (v) { return v.tabId != request.tabId || v.domain != request.domain; });
-        } else if (request.r == 'isExcTab') {
-            for (var i = 0; i < excDomains.length; i++)
-                if (excDomains[i].tabId == request.tabId && excDomains[i].domain == request.domain)
-                    sendResponse(true);
-            sendResponse(false);
-        } else if (request.r == 'pause') {
-            paused = request.pause;
-            localStorage.isPaused = paused ? 1 : 0;
-        } else if (request.r == 'isPaused')
-            sendResponse(paused);
-        else if (request.r == 'setNoPattern') {
-            isNoPattern = request.isNoPattern;
-            localStorage.isNoPattern = isNoPattern ? 1 : 0;
-        } else if (request.r == 'isNoPattern')
-            sendResponse(isNoPattern);
-        else if (request.r == 'setNoEye') {
-            isNoEye = request.isNoEye;
-            localStorage.isNoEye = isNoEye ? 1 : 0;
-        } else if (request.r == 'isNoEye')
-            sendResponse(isNoEye);
-        else if (request.r == 'pauseTab') {
-            if (request.pause)
-                pausedTabs.push(request.tab.id);
-            else
-                pausedTabs = pausedTabs.filter(function (v) { return v != request.tab.id; });
-        } else if (request.r == 'isTabPaused')
-            sendResponse(pausedTabs.indexOf(request.tab.id) != -1);
+                sendResponse(settings);
+                break;
+            case 'setColorIcon':
+                chrome.browserAction.setIcon({ path: request.toggle ? 'icon.png' : 'icon-d.png', tabId: sender.tab.id });
+                break;
+            case 'urlListAdd':
+                var url = request.domainOnly ? getDomain(request.url) : request.url.toLowerCase();
+                if (url) {
+                    urlList.push(url);
+                    chrome.runtime.sendMessage({ r: 'urlListModified' });
+                }
+                break;
+            case 'urlListRemove':
+                if (request.url) {
+                    var lowerUrl = request.url.toLowerCase();
+                    for (var i = 0; i < urlList.length; i++) {
+                        if (lowerUrl.indexOf(urlList[i]) != -1)
+                        { urlList.splice(i, 1); i--; }
+                    }
+                } else
+                    urlList.splice(request.index, 1);
+                chrome.runtime.sendMessage({ r: 'urlListModified' });
+                break;
+            case 'getUrlList':
+                sendResponse(urlList);
+                break;
+            case 'excludeForTab':
+                var domain = getDomain(request.tab.url);
+                if (!domain) return;
+                if (request.toggle) {
+                    excludeForTabList.push({ tabId: request.tab.id, domain: domain });
+                }
+                else {
+                    for (var i = 0; i < excludeForTabList.length; i++)
+                        if (excludeForTabList[i].tabId == request.tab.id && excludeForTabList[i].domain == domain)
+                        { excludeForTabList.splice(i, 1); break; }
+                }
+                break;
+            case 'pause':
+                paused = request.toggle;
+                localStorage.isPaused = paused ? 1 : 0;
+                break;
+            case 'pauseForTab':
+                if (request.toggle)
+                    pauseForTabList.push(request.tabId);
+                else
+                    for (var i = 0; i < pauseForTabList.length; i++)
+                        if (pauseForTabList[i] == request.tabId)
+                        { pauseForTabList.splice(i, 1); break; }
+                break;
+            case 'setNoPattern':
+                isNoPattern = request.toggle;
+                localStorage.isNoPattern = isNoPattern ? 1 : 0;
+                break;
+            case 'setNoEye':
+                isNoEye = request.toggle;
+                localStorage.isNoEye = isNoEye ? 1 : 0;
+                break;
+        }
     }
 );
---
+++ trunk/launch.js
@@ -0,0 +1,9 @@
+﻿var contentLoaded = false;
+window.addEventListener('DOMContentLoaded', function () { contentLoaded = true; });
+
+chrome.runtime.sendMessage({ r: 'getSettings' }, function (settings) {
+    if (!settings.isExcluded && !settings.isExcludedForTab && !settings.isPaused && !settings.isPausedForTab) {
+        chrome.runtime.sendMessage({ r: 'setColorIcon', toggle: true });
+        wzmMain(chrome.extension.getURL(''), settings, contentLoaded);
+    }
+});
