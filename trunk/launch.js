da5fe5babf318f16913d0f3e68ddc6bfcba52ee2
elicymerman <elicymerman@deedef4c-c4cc-42fd-4688-ae2ef018a546>
2015-04-23 11:52:31 +0000

    2.3.0 - restructure code

--- trunk/launch.js
+++
@@ -1,9 +0,0 @@
-﻿var contentLoaded = false;
-window.addEventListener('DOMContentLoaded', function () { contentLoaded = true; });
-
-chrome.runtime.sendMessage({ r: 'getSettings' }, function (settings) {
-    if (settings && !settings.isExcluded && !settings.isExcludedForTab && !settings.isPaused && !settings.isPausedForTab) {
-        chrome.runtime.sendMessage({ r: 'setColorIcon', toggle: true });
-        wzmMain(chrome.extension.getURL(''), settings, contentLoaded);
-    }
-});
